b997583b36284a1fd18e961b40d3da40be5c462d
bp26 <94074742+bp26@users.noreply.github.com>
2023-02-11 20:20:34 +0300

    feat: remove custom errors

--- src/application/utils/errors/validationError.ts
+++
@@ -1,9 +0,0 @@
-import { AuthErrorType } from '../../types/enums';
-
-export class ValidationError extends Error {
-  type: AuthErrorType;
-  constructor(type: AuthErrorType, message: string) {
-    super(message);
-    this.type = type;
-  }
-}
--- src/application/utils/responceError.ts
+++
@@ -1,7 +0,0 @@
-export class ResponceError extends Error {
-  status: string;
-  constructor(status: number, message: string) {
-    super(message);
-    this.status = `${status}`;
-  }
-}
