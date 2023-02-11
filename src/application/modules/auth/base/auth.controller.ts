db68f5da43622ba4c1fb46f5c42b92f6a3d0bb01
bp26 <94074742+bp26@users.noreply.github.com>
2023-02-11 21:19:37 +0300

    feat: update Authontroller

--- src/application/modules/auth/base/auth.controller.ts
+++ src/application/modules/auth/base/auth.controller.ts
@@ -1,3 +1,5 @@
+import { AuthErrorMessage, AuthErrorType } from '../../../types/enums';
+import { LOGIN_LENGTH, PASSWORD_LENGTH } from '../../../utils/constants/constants';
 import { authModel } from './auth.model';
 import { authView } from './auth.view';
 
@@ -6,8 +8,36 @@ class AuthController {
     authView.render();
   }
 
-  public async submit(login: string, password: string): Promise<void> {
-    await authModel.submit(login, password);
+  public showModal(): void {
+    authView.showModal();
+  }
+
+  public submit(login: string, password: string): void {
+    let isValidated = true;
+
+    if (!login || !password) {
+      isValidated = false;
+    }
+    if (login.length < LOGIN_LENGTH || password.length < PASSWORD_LENGTH) {
+      isValidated = false;
+    }
+
+    if (!login) {
+      authView.showError(AuthErrorType.LOGIN, AuthErrorMessage.NO_LOGIN);
+    } else if (login.length < LOGIN_LENGTH) {
+      authView.showError(AuthErrorType.LOGIN, AuthErrorMessage.SHORT_LOGIN);
+    }
+
+    if (!password) {
+      authView.showError(AuthErrorType.PASSWORD, AuthErrorMessage.NO_PASSWORD);
+    } else if (password.length < PASSWORD_LENGTH) {
+      authView.showError(AuthErrorType.PASSWORD, AuthErrorMessage.SHORT_PASSWORD);
+    }
+
+    if (isValidated) {
+      authView.showPreloader();
+      authModel.submit(login, password);
+    }
   }
 
   public switchMode(): void {
