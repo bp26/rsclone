bda1d23d7fb0ec8ff12149df46575288a3fa89d6
bp26 <94074742+bp26@users.noreply.github.com>
2023-02-11 21:22:54 +0300

    feat: load user on start

--- src/application/base/controller.ts
+++ src/application/base/controller.ts
@@ -4,13 +4,23 @@ import { lessonsController } from './../pages/lessons/base/lessons.controller';
 import { headerController } from '../modules/header/base/header.controller';
 import { footerController } from '../modules/footer/base/footer.controller';
 import { homeController } from '../pages/home/base/home.controller';
+import { authController } from '../modules/auth/base/auth.controller';
+import { model } from './model';
+import { preloader } from '../ui/preloader/preloader';
 
 class Controller {
-  public init() {
+  public async init() {
+    preloader.init();
+
     headerController.init();
     footerController.init();
+    authController.init();
     new Sprite();
+
+    await model.init();
+    preloader.hide();
   }
+
   public initHomePage(): void {
     homeController.init();
   }
--- src/application/base/model.ts
+++ src/application/base/model.ts
@@ -1,3 +1,48 @@
-class Model {}
+import { IUser } from '../types/interfaces';
+import { api } from '../api/api';
+import { emitter } from '../utils/emitter';
+import { EmitterEventName } from '../types/enums';
+
+class Model {
+  private user?: IUser;
+
+  constructor() {
+    this.subscribe();
+  }
+
+  public async init(): Promise<void> {
+    const isVerificated = await api.checkVerification();
+    if (isVerificated) {
+      await this.loadUser();
+    }
+  }
+
+  public getUser(): IUser | undefined {
+    return this.user;
+  }
+
+  public async loadUser(): Promise<void> {
+    try {
+      this.user = await api.getUser();
+      emitter.emit(EmitterEventName.GLOBAL_USER_LOAD_SUCCESS, this.user);
+    } catch (error) {
+      emitter.emit(EmitterEventName.GLOBAL_USER_LOAD_ERROR);
+    }
+  }
+
+  public async updateUser() {
+    try {
+      if (this.user) {
+        await api.updateUser(this.user);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  private subscribe() {
+    emitter.on(EmitterEventName.AUTH_SUCCESS, this.loadUser.bind(this));
+  }
+}
 
 export const model = new Model();
