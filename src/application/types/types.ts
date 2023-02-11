e6f15073ea47ad2525f8c5e4325dec65394b46bb
bp26 <94074742+bp26@users.noreply.github.com>
2023-02-11 21:21:01 +0300

    feat: add emitter events

--- src/application/types/types.ts
+++ src/application/types/types.ts
@@ -1,10 +1,15 @@
-import { AuthMode } from './enums';
+import { AuthMode, AuthErrorType } from './enums';
+import { IUser } from './interfaces';
 
 type AuthEvents = {
   AUTH_SWITCH: (mode: AuthMode) => void;
-  AUTH_ERROR: (error: string) => void;
+  AUTH_ERROR: (type: AuthErrorType, error: string) => void;
   AUTH_SUCCESS: () => void;
   AUTH_LOGOUT: () => void;
+  GLOBAL_USER_LOAD_SUCCESS: (user: IUser) => void;
+  GLOBAL_USER_LOAD_ERROR: () => void;
+  GLOBAL_USER_UPDATE_SUCCESS: () => void;
+  GLOBAL_USER_UPDATE_ERROR: () => void;
 };
 
 export type EmitterEvents = AuthEvents;
