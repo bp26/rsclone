8566b37bbfad5ebf724dc07f008071ad53dc03d1
bp26 <94074742+bp26@users.noreply.github.com>
2023-02-11 21:20:37 +0300

    feat: update AuthModel

--- src/application/modules/auth/base/auth.model.ts
+++ src/application/modules/auth/base/auth.model.ts
@@ -1,10 +1,16 @@
-import { EmitterEventName } from '../../../types/enums';
+import { AuthErrorType, EmitterEventName } from '../../../types/enums';
 import { emitter } from '../../../utils/emitter';
 import { AuthMode } from '../../../types/enums';
 import { authApi } from '../api/auth.api';
+import { AxiosError } from 'axios';
+import { ResponceError } from '../../../types/interfaces';
 
 class AuthModel {
-  mode: AuthMode = AuthMode.LOGIN;
+  private mode: AuthMode = AuthMode.LOGIN;
+
+  constructor() {
+    this.subscribe();
+  }
 
   public switchMode(): void {
     switch (this.mode) {
@@ -19,44 +25,51 @@ class AuthModel {
   }
 
   public async submit(login: string, password: string) {
-    switch (this.mode) {
-      case AuthMode.REGISTER:
-        this.register(login, password);
-        break;
-      case AuthMode.LOGIN:
-        this.login(login, password);
-        break;
+    try {
+      switch (this.mode) {
+        case AuthMode.REGISTER:
+          await this.register(login, password);
+          break;
+        case AuthMode.LOGIN:
+          await this.login(login, password);
+          break;
+      }
+      emitter.emit(EmitterEventName.AUTH_SUCCESS);
+    } catch (error) {
+      if (error instanceof AxiosError && error.response?.data) {
+        const responceError: ResponceError = error.response.data;
+        if (responceError.type === AuthErrorType.LOGIN || responceError.type === AuthErrorType.PASSWORD) {
+          emitter.emit(EmitterEventName.AUTH_ERROR, responceError.type, responceError.message);
+        } else {
+          console.log(error);
+        }
+      }
     }
-    emitter.emit(EmitterEventName.AUTH_SUCCESS);
   }
 
   private async register(login: string, password: string): Promise<void> {
-    try {
-      await authApi.register(login, password);
-    } catch (error) {
-      if (error instanceof Error) {
-        emitter.emit(EmitterEventName.AUTH_ERROR, error.message);
-      }
-    }
+    await authApi.register(login, password);
   }
 
   private async login(login: string, password: string): Promise<void> {
-    try {
-      await authApi.register(login, password);
-    } catch (error) {
-      if (error instanceof Error) {
-        emitter.emit(EmitterEventName.AUTH_ERROR, error.message);
-      }
-    }
+    await authApi.login(login, password);
   }
 
   public async logout(): Promise<void> {
     try {
-      await authModel.logout();
-      emitter.emit(EmitterEventName.AUTH_LOGOUT);
+      await authApi.logout();
+      location.replace('/');
     } catch (error) {
       console.log(error);
     }
+  }
+
+  private subscribe() {
+    emitter.on(EmitterEventName.GLOBAL_USER_LOAD_SUCCESS, () => {
+      if (this.mode === AuthMode.REGISTER) {
+        this.switchMode();
+      }
+    });
   }
 }
 
