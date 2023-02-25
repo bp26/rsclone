55f3c15c04c157f54b16f79c3719b19a1274b012
bp26 <94074742+bp26@users.noreply.github.com>
2023-02-25 18:41:55 +0300

    feat: add new user rank sloth

--- src/application/pages/user/data/userRank.ts
+++ src/application/pages/user/data/userRank.ts
@@ -1,5 +1,6 @@
 import { slothGood } from '../../../utils/constants/icons/sloth-good';
 import { slothSamurai } from '../../../utils/constants/icons/sloth-samurai';
 import { BABY_SLOTH } from '../../../utils/constants/icons/baby-sloth';
+import { slothPopcorn } from '../../../utils/constants/icons/sloth-popcorn';
 
-export const userRank = [BABY_SLOTH, slothGood, slothSamurai];
+export const userRank = [BABY_SLOTH, slothPopcorn, slothGood, slothSamurai];
