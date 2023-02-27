86807b3cad56354dc8d3c18787c26a4254d5f92e
bp26 <94074742+bp26@users.noreply.github.com>
2023-02-27 18:45:35 +0300

    feat: remove theme from header

--- src/application/modules/header/base/header.controller.ts
+++ src/application/modules/header/base/header.controller.ts
@@ -1,6 +1,5 @@
 import { headerView } from './header.view';
 import { model } from '../../../base/model';
-import { Theme } from '../../../types/enums';
 
 class HeaderController {
   public init(): void {
@@ -9,10 +8,6 @@ class HeaderController {
     if (model.isAuthenticated && model.user) {
       headerView.renderSignedAuth(model.user);
     }
-  }
-
-  public setTheme(theme: Theme): void {
-    model.setTheme(theme);
   }
 }
 
